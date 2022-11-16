676722310bf5f11956b06fa4989d5dd2dbdcea92
Brandon2532 <pruib01@pfw.edu>
2022-11-15 19:33:17 -0500

    created dummy information for device-backend

--- device-backend/server/main.js
+++ device-backend/server/main.js
@@ -7,6 +7,28 @@ Meteor.startup(() => {
   // code to run on server at startup
   deviceInformationdb.remove({});
 
+  const locations = ["Reception", "Dermatology", "General Practitioner", "Lab"];
+
+
+
+  //random number
+  const getRandomNumber = (max) => Math.floor(Math.random() * max);
+  
+  
+  //getting random names
+  function getRandomLocation(arr1) {
+  
+    let location = arr1[getRandomNumber(arr1.length)];
+   
+  
+    
+  
+    return location;
+  }
+  
+
+
+
   for (let i = 0; i < 6; i++) {
     var patientID = "XXXXXXX".replace(/X/g, function () {
       return "0123456789ABCDEF".charAt((Math.random() * 16))
@@ -14,82 +36,55 @@ Meteor.startup(() => {
     var macAddress = "XX:XX:XX:XX:XX:XX".replace(/X/g, function () {
       return "0123456789ABCDEF".charAt((Math.random() * 16))
     });
-    var macAddress = "XX:XX:XX:XX:XX:XX".replace(/X/g, function () {
-      return "0123456789ABCDEF".charAt((Math.random() * 16))
-    });
-
-    const firstNames = ["Michael", "Christopher", "Jessica", "Matthew", "Ashley", "Jennifer", "Joshua", "Amanda", "Daniel", "David", "James", "Robert", "John",
-  "Joseph", "Andrew", "Ryan", "Brandon", "Jason", "Justin", "Sarah", "William", "Jonathan", "Stephanie", "Brian", "Nicole", "Nicholas", "Anthony", "Heather",
-  "Eric", "Elizabeth", "Adam", "Megan", "Melissa", "Kevin", "Steven", "Thomas", "Timothy", "Christina", "Kyle", "Rachel", "Laura", "Lauren", "Amber", "Brittany",
-  "Danielle", "Richard", "Kimberly", "Jeffrey", "Amy", "Crystal", "Michelle", "Tiffany", "Jeremy", "Benjamin", "Mark", "Emily", "Aaron"];
-
-const lastNames = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzalez",
-  "Wilson", "Anderson", "Thomas", "Taylor", "Moore", "Jackson", "Martin", "Lee", "Perez", "Thompson", "White", "Harris", "Sanchez", "Clark", "Ramirez", "Lewis",
-  "Robinson", "Walker", "Young", "Allen", "King", "Wright", "Scott", "Torres", "Nguyen", "Hill", "Flores", "Green", "Adams", "Nelson", "Baker", "Hall", "Rivera",
-  "Campbell", "Mitchell", "Carter", "Roberts"];
-
-  const getRandomNumber = (max) => Math.floor(Math.random() * max);
+    function getTimestampInSeconds () {
+      return Math.floor(Date.now() / 1000)
+    }
+    
+    
+    var deviceID=i;
 
 
-//getting random names
-function getRandomName(arr1, arr2) {
-
-  let firstName = arr1[getRandomNumber(arr1.length)];
-  let lastName = arr2[getRandomNumber(arr2.length)];
-
-  let name = firstName + " " + lastName;
-
-  return name;
-}
-
-//generate random DoB
-function randomDate(start, end) {
-  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toDateString();
-}
-
-//generate age
-function ageCalculator(DOB) {
-  var dob = new Date(DOB);
-
-  //calculate month difference from current date in time
-  var month_diff = Date.now() - dob.getTime();
-
-  //convert the calculated difference in date format
-  var age_dt = new Date(month_diff);
-
-  //extract year from date    
-  var year = age_dt.getUTCFullYear();
-
-  //now calculate the age of the user
-  var age = Math.abs(year - 1970);
-
-  //display the calculated age
-  return age;
-}
-
- 
 
 
-    var DOB = randomDate(new Date(1970, 0, 1), new Date());
+     var timeStamp=getTimestampInSeconds();
 
-    var age = ageCalculator(DOB);
 
-    var physicianName = "Dr. " + getRandomName(firstNames, lastNames);
+    
 
-    //random number
+
+     var location=getRandomLocation(locations);
+   
+
+    
 
     deviceInformationdb.insert({
-
       
-      "patientInformation": {
+        "deviceID": deviceID,
         "macAddress": macAddress,
+        "location": location,
         "patientID": patientID,
-        "DOB": DOB,
-        "age": age,
-        "physicianName": physicianName
-      }
+        "timeStamp":timeStamp,
+        
 
     });
+
+
+
+
+
+
   }
+
+
+
+
+ 
+  
+
+
+
+
+
+
 });
 
